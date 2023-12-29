9d4626383c4a134258f01b4cdbf85033f7766332
Florian Lepage <florian.kyn@gmail.com>
2023-12-29 14:59:32 +0100

    Update server.ts

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,11 +1,11 @@
-import { z } from 'zod';
+import { z } from 'zod'
 
 const envVariables = z.object({
   MASTER_TOKEN_SECRET: z.string(),
-  DATABASE_URL: z.string()
-});
+  DATABASE_URL: z.string(),
+})
 
-envVariables.parse(process.env);
+envVariables.parse(process.env)
 
 declare global {
   namespace NodeJS {
@@ -13,19 +13,19 @@ declare global {
   }
 }
 
-import express from 'express';
+import express from 'express'
 
-const app = express();
+const app = express()
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '10mb' }))
 
 // import routes
-import routerV1 from '../api/v1/routes/loader';
-app.use(routerV1);
+import routerV1 from '../api/v1/routes/loader'
+app.use(routerV1)
 
 // start server
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+  console.log(`Server listening on port ${PORT}`)
+})
