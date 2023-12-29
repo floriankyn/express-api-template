41f3a7bdf4df432be600d5cfbe64e90355c8165e
Florian Lepage <florian.kyn@gmail.com>
2023-12-29 14:59:16 +0100

    Update pingController.ts

--- src/api/v1/controllers/pingController.ts
+++ src/api/v1/controllers/pingController.ts
@@ -1,25 +1,25 @@
-import { Request, Response } from 'express';
+import { Request, Response } from 'express'
 
 export const get = async (req: Request, res: Response): Promise<Response> => {
   return res.status(200).json({
     message: 'Get pong',
-  });
-};
+  })
+}
 
 export const post = async (req: Request, res: Response): Promise<Response> => {
   return res.status(200).json({
     message: 'Post pong',
-  });
-};
+  })
+}
 
 export const del = async (req: Request, res: Response): Promise<Response> => {
   return res.status(200).json({
     message: 'Delete pong',
-  });
-};
+  })
+}
 
 export const put = async (req: Request, res: Response): Promise<Response> => {
   return res.status(200).json({
     message: 'Put pong',
-  });
-};
+  })
+}
