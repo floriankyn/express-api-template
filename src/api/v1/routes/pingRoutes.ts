a97f62ea8a618768bfbfb6d6b95745669f5c9208
Florian Lepage <florian.kyn@gmail.com>
2023-12-29 14:59:21 +0100

    Update pingRoutes.ts

--- src/api/v1/routes/pingRoutes.ts
+++ src/api/v1/routes/pingRoutes.ts
@@ -1,11 +1,11 @@
 // ping.ts//
-import express, { Router } from 'express';
-const router: Router = express.Router();
-import { get, post, del, put } from '../controllers/pingController.js';
+import express, { Router } from 'express'
+const router: Router = express.Router()
+import { get, post, del, put } from '../controllers/pingController.js'
 
-router.get('/api/v1/ping', get);
-router.post('/api/v1/ping', post);
-router.delete('/api/v1/ping', del);
-router.put('/api/v1/ping', put);
+router.get('/api/v1/ping', get)
+router.post('/api/v1/ping', post)
+router.delete('/api/v1/ping', del)
+router.put('/api/v1/ping', put)
 
-export { router };
+export { router }
