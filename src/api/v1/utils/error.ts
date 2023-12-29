a62183f74be78f57ed0c3ff285f0c4fdee63253e
Florian Lepage <florian.kyn@gmail.com>
2023-12-29 15:51:15 +0100

    Create error.ts

---
+++ src/api/v1/utils/error.ts
@@ -0,0 +1,18 @@
+import { Response } from 'express';
+
+export const errorFormattedResponse = (
+  res: Response,
+  status: number,
+  errorMsg: string,
+  details: string | null = null,
+  trailString: string | null = null,
+) => {
+  res.status(status).json({
+    timestamp: new Date().toISOString(),
+    status: status,
+    error: errorMsg,
+    details: details,
+    path: res.req.url,
+    trailStack: trailString
+  });
+}
