8ff69968a5ce5f51cd04eebe3695a4c6993f1679
Florian Lepage <florian.kyn@gmail.com>
2023-12-29 15:51:10 +0100

    Update server.ts

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,4 +1,6 @@
 import { z } from 'zod'
+import morgan from 'morgan';
+import { logMessage } from '../api/v1/utils/logger'
 
 const envVariables = z.object({
   MASTER_TOKEN_SECRET: z.string(),
@@ -21,9 +23,18 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json({ limit: '10mb' }))
 
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms", {
+    stream: {
+      write: (message) => logMessage(message.trim())
+    }
+  })
+)
+
+
 // import routes
 import routerV1 from '../api/v1/routes/loader'
 app.use(routerV1)
+
 
 // start server
 app.listen(PORT, () => {
